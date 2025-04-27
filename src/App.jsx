ad9e631ff480e27e23acf7e8f934f8f6eed1ab03
Farheen Malak <115576023+FarheenMalak@users.noreply.github.com>
2025-04-27 14:51:13 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
-import Home from "./Components/Home"; // apna simple Home component bana lena
+import Home from "./Components/Home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignUp />} />
-        <Route path="/login" element={Login} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
@@ -17,3 +17,4 @@ const App = () => {
 };
 
 export default App;
+
